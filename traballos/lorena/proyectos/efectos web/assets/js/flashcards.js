968d72fcbbfded66d904f6d096230ec18b36f9e7
XuventudeTerrasDeIria2023 <149288493+XuventudeTerrasDeIria2023@users.noreply.github.com>
2024-09-12 10:33:44 +0200

    Delete traballos/lorena/proyectos/efectos web directory

--- traballos/lorena/proyectos/efectos web/assets/js/flashcards.js
+++
@@ -1,7 +0,0 @@
-/**
- * Invoked when a flashcard element has been clicked on.
- * @param {HTMLElement} flashcard The div.flashcard element that was clicked on
- */
-function flip(flashcard) {
-  flashcard.classList.toggle('flashcard-flipped');
-}
--- traballos/lorena/proyectos/efectos web/assets/js/img-seleccion.js
+++
@@ -1,4 +0,0 @@
-var x = $(document);
-x. ready(inicia);
-function inicia(){
- $(“imagenes .secuenciales).each(function(secuencia){   $(this).delay((secuenci)*1500).fadeTo)1500,1)})
