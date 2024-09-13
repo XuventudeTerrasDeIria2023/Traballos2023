c7840299aba6bb0c6cf125207298c24d3483916c
XuventudeTerrasDeIria2023 <149288493+XuventudeTerrasDeIria2023@users.noreply.github.com>
2024-09-13 09:34:36 +0200

    Add files via upload

---
+++ traballos/PaulaFM/Friro-dance/assets/js/trabaja.js
@@ -0,0 +1,4 @@
+document.getElementById('uploadForm').addEventListener('submit', function(event) {
+    event.preventDefault(); 
+    document.getElementById('message').style.display = 'block';
+});
